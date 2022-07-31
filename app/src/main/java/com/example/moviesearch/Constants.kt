0906ff3242df2bb8a4302c90d4a78570da9cea40
gitddabong <wonjong0701@naver.com>
2022-07-31 20:57:31 +0900

    gitignore 일부 수정

--- app/src/main/java/com/example/moviesearch/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.example.moviesearch
-
-val clientId = "H8nXnpefveSI2x5pOyjd"
-val clientSecret = "hNslSZqruK"
-val BASE_URL = "https://openapi.naver.com/v1/"
-val TAG = "test"
-val tableName = "recent_words"
